979f9a6b7c3b17a4bb61f49301fe6f167834bba6
Leo Belyi <leonid@ac93.org>
2018-09-29 12:34:11 -0400

    Fix empty string and empty string set create problem

--- src/main/java/databaseOperations/CreateDatabaseAction.java
+++ src/main/java/databaseOperations/CreateDatabaseAction.java
@@ -2,11 +2,25 @@ package main.java.databaseOperations;
 
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class CreateDatabaseAction extends DatabaseAction {
     public CreateDatabaseAction(Map<String, AttributeValue> item) {
         action = DBAction.CREATE;
-        this.item = item;
+        this.item = new HashMap<>();
+        for (Map.Entry<String, AttributeValue> entry: item.entrySet()) {
+            String key = entry.getKey();
+            AttributeValue attributeValue = entry.getValue();
+            if (attributeValue != null) {
+                if ((attributeValue.getS() != null && attributeValue.getS().equals("")) || (attributeValue.getSS() !=
+                        null && attributeValue.getSS().size() == 0)) {
+                    this.item.put(key, null);
+                }
+                else {
+                    this.item.put(key, attributeValue);
+                }
+            }
+        }
     }
 }
