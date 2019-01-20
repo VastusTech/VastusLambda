a37bbcbbc26744a871ccfd78a61838b8bee1cde9
Leo Belyi <leonid@ac93.org>
2019-01-20 11:57:29 -0500

    Add PING

--- src/main/java/lambdaFunctionHandlers/LambdaFunctionHandler.java
+++ src/main/java/lambdaFunctionHandlers/LambdaFunctionHandler.java
@@ -6,6 +6,10 @@ import main.java.Logic.Constants;
 public class LambdaFunctionHandler {
     public LambdaResponse handleRequest(LambdaRequest input, Context context) throws Exception {
         // LambdaResponse is initialized at the very end, before the return is sent
+        // Put in a ping response to return as quickly as possible
+        if (input.getAction() != null && input.getAction().equals("PING")) {
+            return new LambdaResponse(null);
+        }
         Constants.setLogger(context.getLogger());
         Constants.debugLog(input.toString());
         return new LambdaResponse(input.process());
