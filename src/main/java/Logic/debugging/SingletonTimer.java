87d4700a0fc4e8496fec50236593b5f72af371aa
Leo Belyi <leonid@ac93.org>
2019-03-21 13:24:55 -0400

    Javadoc Timer and SingletonTimer

--- src/main/java/Logic/debugging/SingletonTimer.java
+++ src/main/java/Logic/debugging/SingletonTimer.java
@@ -1,8 +1,16 @@
 package main.java.Logic.debugging;
 
+/**
+ * Wraps the {@link Timer} class in a singleton class wrapper so that we can use the same timer for
+ * the whole program.
+ */
 public class SingletonTimer extends Timer {
     private static SingletonTimer instance;
 
+    /**
+     * Singleton pattern getter for the class.
+     * @return The global instance for the SingletonTimer.
+     */
     public static synchronized SingletonTimer get() {
         if (instance == null) {
             instance = new SingletonTimer();
@@ -10,5 +18,8 @@ public class SingletonTimer extends Timer {
         return instance;
     }
 
+    /**
+     * Private constructor for the singleton pattern.
+     */
     private SingletonTimer() { super(); }
 }
--- src/main/java/Logic/debugging/Timer.java
+++ src/main/java/Logic/debugging/Timer.java
@@ -3,30 +3,52 @@ package main.java.Logic.debugging;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * This class is designed to help do complete time testing and pretty printing of the time results.
+ */
 public class Timer {
     private Map<String, Long> times;
     private long startTime = -1;
     private long endTime = -1;
 
-
+    /**
+     * Constructor for the Timer class, simply initializes the times map.
+     */
     public Timer() {
         times = new LinkedHashMap<>();
     }
 
+    /**
+     * Starts the total timer and adds a section to the times.
+     * @param startSectionName The name of the first section being timed.
+     */
     public void start(String startSectionName) {
         startTime = checkpoint(startSectionName);
     }
 
+    /**
+     * Puts another section into the times map with a section name to it.
+     * @param nextSectionName The name of the next section to be timed.
+     * @return The current time in milliseconds.
+     */
     public long checkpoint(String nextSectionName) {
         long time = System.currentTimeMillis();
         times.put(nextSectionName, time);
         return time;
     }
 
+    /**
+     * Ends the total timer and adds a final section to the times.
+     */
     public void finish() {
         endTime = checkpoint("end");
     }
 
+    /**
+     * Gives all the diagnostic time-based information for the timer, including which sections took
+     * the longest and potentially where we can cut down on execution time!
+     * @return The string of the diagnostic report.
+     */
     @Override
     public String toString() {
         if (startTime == -1 || endTime == -1) {
